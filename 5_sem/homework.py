ca80b040d0b8bdd7157ce9462ba58a411c7636df
Safar9808 <safik686@mail.ru>
2023-02-03 19:47:56 +0400

    доделал небольшую часть

--- 5_sem/homework.py
+++ 5_sem/homework.py
@@ -1,8 +1,21 @@
-# Создайте программу для игры с конфетами человек против человека
+# Создайте программу для игры с конфетами человек против бота без интеллекта
 
 from random import randint
 s = int(input("Enter the number of candies: "))
-queue= randint(1,3)
+queue= randint(1,3)                                 
 print(f'Now the turn is {"HUMAN" if queue==0 else "BOT"}')
+while s>0:
+    if queue==0:
+        a=int(input("HUMAN, how math do you take?: "))
+        print(f'HUMAN take {a} candies, balance is {s-a} candies')
+        s=s-a
+        queue=1
+    else: 
+        a=randint(1,29)
+        print(f'BOT take {a} candies, balance is {s-a} candies')
+        s=s-a
+        queue=0
+print(f'Winner is {"HUMAN" if queue==1 else "BOT"}')
 
-# только начал писать, время от времени буду пушить в гитхаб
+
+# решение в процессе, пушшу время от времени
