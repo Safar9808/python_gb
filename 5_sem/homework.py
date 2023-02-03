81a38131732fdd68af18ae03b3633754ac610969
Safar9808 <safik686@mail.ru>
2023-02-03 20:28:20 +0400

    доделал игру с конфетами

--- 5_sem/homework.py
+++ 5_sem/homework.py
@@ -1,21 +1,46 @@
-# Создайте программу для игры с конфетами человек против бота без интеллекта
+# Программа для игры с конфетами человек против бота без интеллекта
 
 from random import randint
-s = int(input("Enter the number of candies: "))
-queue= randint(1,3)                                 
-print(f'Now the turn is {"HUMAN" if queue==0 else "BOT"}')
-while s>0:
-    if queue==0:
-        a=int(input("HUMAN, how math do you take?: "))
-        print(f'HUMAN take {a} candies, balance is {s-a} candies')
-        s=s-a
-        queue=1
-    else: 
-        a=randint(1,29)
-        print(f'BOT take {a} candies, balance is {s-a} candies')
-        s=s-a
-        queue=0
-print(f'Winner is {"HUMAN" if queue==1 else "BOT"}')
 
+"""
+s = int(input("Enter the number of candies: "))
+queue = randint(1, 3)
+print(f'Now the turn is {"HUMAN" if queue==0 else "BOT"}')
+while s > 0:
+    if queue == 0:
+        a = int(input("HUMAN, how math do you take?: "))
+        print(f'HUMAN take {a} candies, balance is {s-a} candies')
+        s = s-a
+        queue = 1
+    else:
+        a = randint(1, 29)
+        print(f'BOT take {a} candies, balance is {s-a} candies')
+        s = s-a
+        queue = 0
+print(f'Winner is {"HUMAN" if queue==1 else "BOT"}')
+"""
+
+# Программа для игры с конфетами человек против бота c интеллектом ( порвет любого ) 
+
+s = int(input("Enter the number of candies: "))
+queue = randint(1, 3)
+print(f'Now the turn is {"HUMAN" if queue==0 else "BOT"}')
+while s > 0:
+    if queue == 0:
+        a = int(input("HUMAN, how math do you take?: "))
+        print(f'HUMAN take {a} candies, balance is {s-a} candies')
+        s = s-a
+        queue = 1
+    else:
+        if s <= 28:
+            a = s
+        elif ((s-2)//28) % 2 == 1:
+            a = (s % 28-1) if s % 28 > 2 else 27+s % 28
+        else:
+            a = 1
+        print(f'BOT take {a} candies, balance is {s-a} candies')
+        s = s-a
+        queue = 0
+print(f'Winner is {"HUMAN" if queue==1 else "BOT"}')
 
 # решение в процессе, пушшу время от времени
